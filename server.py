6c06d50ffdb53383ef269b874447683cfb81d4a7
Max <maximilian.bolt@student.manchester.ac.uk>
2024-07-31 12:49:50 +0100

    basic page

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
 # to start server, type python server.py into command line
-from flask import Flask  # type: ignore
+from flask import Flask, render_template, request  # type: ignore
 import AI_Response
 
 app = Flask(__name__)
 
 @app.route('/')
 def initialise():
-    response = AI_Response.getResponse("You are to repeat back, it works!") #test basic ai response
-    return response['text']
+    response = AI_Response.getResponse("In two sentances or less, introduce yourself as  BOT-T, who will answer and questions about Max or his cv in plain text") #test basic ai response
+    return render_template('home.html', response=response['text'])
+
+@app.route('/', methods=['POST'])
+def respond():
+    response = AI_Response.getResponse(request.form['input']) #answer user question
+    return render_template('home.html', response=response['text'])
 
 if __name__ == '__main__':
     app.run(host='localhost', port = 80) #use port 80 to make link "localhost"
